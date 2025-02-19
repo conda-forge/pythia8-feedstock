18589387e3c72eff223f34f59818f039ee44c80e
Matthew Feickert <matthew.feickert@cern.ch>
2025-02-18 17:44:00 -0700

    exit early to make script cleaner

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -111,55 +111,57 @@ make clean
 "$CXX" main101.cc -o main101 $(pythia8-config --cxxflags --ldflags)
 ./main101 &> main101_output.txt || ./main101
 
+# Exit early under emulation
 if [[ "${build_platform}" != "${target_platform}" ]]; then
     echo -e "\n# Target platform ${target_platform} requires emulation on"\
         "${build_platform} runners and will be too slow to run the full tests,"\
         "and so is being skipped."
-else
-    echo -e "\n# Test example main161 that uses the FastJet library extension"
-    make clean
-    # avoid lots of output to stdout from download of the file
-    lhapdf install cteq6l1 &> /dev/null
-
-    "$CXX" main161.cc -o main161 $CXXFLAGS $LDFLAGS -lpythia8 -lfastjet
-    ./main161 main161.cmnd w+_production_lhc_0.lhe histout161.dat &> main161_output.txt || ./main161 main161.cmnd w+_production_lhc_0.lhe histout161.dat
-
-    echo -e "\n# Test example main201 that uses the LHAPDF library extension"
-    make clean
-    # avoid lots of output to stdout from download of the file
-    lhapdf install NNPDF31_nnlo_as_0118_luxqed &> /dev/null
-
-    # The examples assume you built locally and didn't install, so need to patch
-    # the relative path (../share) to the absolute path ($PREFIX/share)
-    sed -i "s|../share|$(readlink -f $PREFIX/share)|g" main201.cc
-
-    "$CXX" main201.cc -o main201 $(pythia8-config --cxxflags --ldflags)
-    ./main201 &> main201_output.txt || ./main201
-
-    echo -e "\n# Test example main212 that uses the FastJet library extension"
-    make clean
-
-    "$CXX" main212.cc -o main212 $CXXFLAGS $LDFLAGS -lpythia8 -lfastjet
-    ./main212 &> main212_output.txt || ./main212
-
-    echo -e "\n# Test example that use the HepMC2 and HepMC3 extensions"
-    "$CXX" main131.cc -o main131 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
-    ./main131 &> main131_output.txt || ./main131
-    test -f main131.hepmc
-
-    "$CXX" main132.cc -o main132 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
-    ./main132 main132.cmnd main132.hepmc &> main132_output.txt || ./main132 main132.cmnd main132.hepmc
-    test -f main132.hepmc
-
-    "$CXX" main133.cc -o main133 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
-    ./main133 main133.cmnd main133.hepmc &> main133_output.txt || ./main133 main133.cmnd main133.hepmc
-    test -f main133.hepmc
-
-    "$CXX" main134.cc -o main134 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
-    ./main134 main134.cmnd main134.hepmc &> main134_output.txt || ./main134 main134.cmnd main134.hepmc
-    test -f main134.hepmc
-
-    "$CXX" main135.cc -o main135 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
-    ./main135 &> main135_output.txt || ./main135
-    test -f main135.hepmc
+    exit 0
 fi
+
+echo -e "\n# Test example main161 that uses the FastJet library extension"
+make clean
+# avoid lots of output to stdout from download of the file
+lhapdf install cteq6l1 &> /dev/null
+
+"$CXX" main161.cc -o main161 $CXXFLAGS $LDFLAGS -lpythia8 -lfastjet
+./main161 main161.cmnd w+_production_lhc_0.lhe histout161.dat &> main161_output.txt || ./main161 main161.cmnd w+_production_lhc_0.lhe histout161.dat
+
+echo -e "\n# Test example main201 that uses the LHAPDF library extension"
+make clean
+# avoid lots of output to stdout from download of the file
+lhapdf install NNPDF31_nnlo_as_0118_luxqed &> /dev/null
+
+# The examples assume you built locally and didn't install, so need to patch
+# the relative path (../share) to the absolute path ($PREFIX/share)
+sed -i "s|../share|$(readlink -f $PREFIX/share)|g" main201.cc
+
+"$CXX" main201.cc -o main201 $(pythia8-config --cxxflags --ldflags)
+./main201 &> main201_output.txt || ./main201
+
+echo -e "\n# Test example main212 that uses the FastJet library extension"
+make clean
+
+"$CXX" main212.cc -o main212 $CXXFLAGS $LDFLAGS -lpythia8 -lfastjet
+./main212 &> main212_output.txt || ./main212
+
+echo -e "\n# Test example that use the HepMC2 and HepMC3 extensions"
+"$CXX" main131.cc -o main131 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
+./main131 &> main131_output.txt || ./main131
+test -f main131.hepmc
+
+"$CXX" main132.cc -o main132 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
+./main132 main132.cmnd main132.hepmc &> main132_output.txt || ./main132 main132.cmnd main132.hepmc
+test -f main132.hepmc
+
+"$CXX" main133.cc -o main133 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
+./main133 main133.cmnd main133.hepmc &> main133_output.txt || ./main133 main133.cmnd main133.hepmc
+test -f main133.hepmc
+
+"$CXX" main134.cc -o main134 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
+./main134 main134.cmnd main134.hepmc &> main134_output.txt || ./main134 main134.cmnd main134.hepmc
+test -f main134.hepmc
+
+"$CXX" main135.cc -o main135 $CXXFLAGS $LDFLAGS -lpythia8 -lHepMC3
+./main135 &> main135_output.txt || ./main135
+test -f main135.hepmc
